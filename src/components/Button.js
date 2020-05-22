b0209087f605e3108f62c2526abdbb3014ae89e6
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-05-22 13:24:18 +0900

    버튼 컴포넌트 type 속성 설정할 수 있도록 수정.

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,20 +3,46 @@ import "./Button.scss";
 
 //****Button 컴포넌트****//
 //props
+//type : 버튼의 type / type : String
 //value : 버튼에 표시할 텍스트 / type : String
 //onClick : 버튼 클릭시 실행할 콜백 메소드
 
 class Button extends Component {
   onBtnClickListener(e) {
     let { onClick } = this.props;
-    onClick();
+
+    if (onClick !== undefined) {
+      onClick();
+    }
+  }
+
+  getType() {
+    let { type } = this.props;
+    if (type === undefined) {
+      return "";
+    }
+
+    return type;
+  }
+
+  getValue() {
+    let { value } = this.props;
+    if (value === undefined) {
+      return "";
+    }
+
+    return value;
   }
 
   render() {
-    let { value } = this.props;
+    let value = this.getValue();
+    let type = this.getType();
+
     return (
       <button
+        type={type}
         onClick={this.onBtnClickListener.bind(this)}
+        // onSubmit={}
         className="kohub-btn"
       >
         {value}
