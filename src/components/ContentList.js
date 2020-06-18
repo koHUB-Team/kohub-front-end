dfda1247b63282d25797865ea5a311837e635065
DESKTOP-M2F1R3B\WooJin <sscj4459@naver.com>
2020-06-18 10:25:19 +0900

    ContentList 컴포넌트 content 리스트에 링크를 통한 라우팅이 가능하도록 옵션 추가

--- src/components/ContentList.js
+++ src/components/ContentList.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "./ContentList.scss";
+import { Link } from "react-router-dom";
 
 //props
 //title : 리스트 상단 타이틀 값 / type : string
 //contents : 리스트에 보여줄 데이터 / type : immutable.List
+//moreLink: 더보기 클릭시 이동할 링크
 
 class ContentList extends Component {
   constructor(props) {
@@ -21,11 +23,21 @@ class ContentList extends Component {
 
   getContentList() {
     let { contents } = this.props;
+    let link = this.getLink();
+
     if (contents !== undefined) {
       let contentList = contents.reduce((acc, contentData) => {
         return acc.concat([
-          <li key={contentData.id}>
-            <span>{contentData.title}</span>
+          <li key={contentData.id} data-id={contentData.id}>
+            <div className="kohub-content-list__title-area">
+              <Link to={`${link}/${contentData.id}`}>
+                <span className="kohub-content-list__title">
+                  {contentData.title}
+                </span>
+              </Link>
+            </div>
+
+            <span className="kohub-content-list__date">{contentData.date}</span>
           </li>,
         ]);
       }, []);
@@ -35,9 +47,19 @@ class ContentList extends Component {
     return [];
   }
 
+  getLink() {
+    let { link } = this.props;
+    if (link !== undefined) {
+      return link;
+    }
+
+    return "";
+  }
+
   render() {
     let title = this.getTitle();
     let contentList = this.getContentList();
+    let link = this.getLink();
 
     return (
       <div className="kohub-content-list">
@@ -46,11 +68,17 @@ class ContentList extends Component {
             <h2>{title}</h2>
           </div>
           <div className="kohub-content-list__more">
-            <span>더보기</span>
+            <Link to={link}>
+              <span>더보기</span>
+            </Link>
           </div>
         </div>
         <div className="kohub-conent-list__contents">
-          <ul>{contentList}</ul>
+          <ul
+          // onClick={this.onContentClickListener.bind(this)}
+          >
+            {contentList}
+          </ul>
         </div>
       </div>
     );
